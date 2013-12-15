886f2b7dc1e1c24f8119eaafd84616a674caef84
Tim <timothybrettgallant@gmail.com>
2013-12-15 01:45:19 -0700

    reworked setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
-# -*- coding: utf-8 -*-
-
-import os
-from setuptools import setup, find_packages
-from imp import load_source
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
 
 with open('README.md') as file:
-	long_description = file.read()
+    long_description = file.read()
 
 setup(name='pyteaser',
-    version='1.0',
-    description="PyTeaser is based on the original TextTeaser project written in Scala by Mojojolo. It's completely re-written in Python.",
-    long_description=long_description,
-    license='MIT',
-    package_dir = {'': "python-goose"},
-    packages=['goose', 'goose.utils', 'goose.images', 'goose.resources', 'goose.videos'],
-    include_package_data=True,
-    install_requires=['Pillow', 'lxml', 'cssselect', 'jieba', 'beautifulsoup'],
-    test_suite="tests"
-)
+      version='1.0',
+      description="PyTeaser is based on the original TextTeaser project written in Scala by Mojojolo. It's completely re-written in Python.",
+      long_description=long_description,
+      license='MIT',
+      install_requires=['Pillow', 'lxml', 'cssselect', 'jieba', 'beautifulsoup'],
+      packages=find_packages(),
+      py_modules=['pyteaser'],
+      package_data={'goose': ['resources/images/*', 'resources/text/*']},
+      test_suite='tests')
